8af3fc22d555aae0767a230328a19afb2a75d516
Vivatsathorn Thitasirivit <vivatsathorn@outlook.co.th>
2024-12-21 17:05:02 +0700

    New

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,13 @@
 #include <Arduino.h>
-//#include "SoftwareSerial.h"
 #include "vt_lora"
 
 using namespace vt;
 using lora_t = lora_e22;
 
-HardwareSerial Serial2(PA3, PA2);
+// HardwareSerial Serial1(PA10, PA9);
 
-lora_t lora_tx(Serial1, PA0, PA1);
-lora_t lora_rx(Serial2, PA5, PA6);
+lora_t lora_tx(Serial1, PA5, PA6);
+// lora_t lora_rx(Serial2, PA5, PA6);
 
 void setup() {
     delay(5000);
@@ -16,7 +15,7 @@ void setup() {
     Serial.begin();
 
     // Begin configuration (sleep) mode.
-    lora_t *loras[] = {&lora_tx, &lora_rx};
+    lora_t *loras[] = {&lora_tx};
     for (auto &lora: loras) {
         Serial.println("Beginning configuration mode...");
         lora->config();
@@ -36,18 +35,17 @@ void setup() {
     }
 
     lora_tx.begin(115200);
-    lora_rx.begin(115200);
 }
 
 void loop() {
     digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-    Serial.println("Sent!");
+    // Serial.println("Sent!");
 
     // Test tx
-    lora_tx.hw_serial().println("Message");
+    // lora_tx.hw_serial().println("Message");
 
     // Test rx
-    while (lora_rx.hw_serial().available()) Serial.write(lora_rx.hw_serial().read());
+    // while (lora_rx.hw_serial().available()) Serial.write(lora_rx.hw_serial().read());
 
     delay(1000);
 }
